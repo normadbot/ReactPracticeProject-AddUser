8fd84449d9335ab179875e8a3b1f7b7650cf1dcf
Sarabjeet <sabysingh298@gmail.com>
2021-04-18 23:32:34 +0530

    Deleted and Added CSS files

--- src/Components/Users/UserList.js
+++ src/Components/Users/UserList.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 import Card from '../UI/Card';
-import classes from './UsersList.module.css';
+import classes from './UserList.module.css';
 
 const UsersList = (props) => {
   return (
